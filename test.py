6ac072277fdef8c01857c0b67c989ce9d07b0afc
anasedf <153298034+anasedf@users.noreply.github.com>
2025-03-02 23:24:34 +0700

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/predict"
-with open('./upload_images/test.jpg', 'rb') as f:
-    response = requests.post(url, files={'file': f})
-print(response.json())
-
